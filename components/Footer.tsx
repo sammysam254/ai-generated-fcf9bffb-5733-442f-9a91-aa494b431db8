94db9a4047b61a74ed029a8c842dc79fa0a9d400
sammysam254 <Sammyseth260@gmail.com>
2025-10-19 22:14:44 +0300

    Add components/Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 p-5 shadow-lg mt-10">
+      <p>&copy; 2024 My Portfolio Website</p>
+    </footer>
+  );
+};
+
+export default Footer;
