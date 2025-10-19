95e17f970a863f3771873d2b40bb37aab762b03f
sammysam254 <Sammyseth260@gmail.com>
2025-10-19 22:14:43 +0300

    Add lib/supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseSecret = process.env.SUPABASE_SECRET;
+
+const supabase = createClient(supabaseUrl, supabaseKey, supabaseSecret);
+
+export { supabase };
